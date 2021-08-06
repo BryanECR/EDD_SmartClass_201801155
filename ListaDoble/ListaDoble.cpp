763670de61ecdf83ad66d5af331bf94951a8a3aa
BryanECR <70345203+BryanECR@users.noreply.github.com>
2021-08-05 23:31:54 -0500

    funciones
    se implementarion las funciones de modificar y eliminar nodo para la lista de tareas

--- ListaDoble/ListaDoble.cpp
+++ ListaDoble/ListaDoble.cpp
@@ -39,19 +39,19 @@ class ListaDoblementeEnlazada{
     }
 
     void mostrar(){
-        Nodo* reco = inicio;
-        while(reco!= NULL){
-            cout<< reco->carnet <<'\t';
-            cout<< reco->nombre <<'\t';
-            cout<< reco->descripcion <<'\t';
-            cout<< reco->materia <<'\t';
-            cout<< reco->fecha <<'\t';
-            cout<< reco->hora <<'\t';
-            cout<< reco->estado <<endl;
-            reco = reco->der;
+        Nodo* actual = inicio;
+        while(actual!= NULL){
+            cout<< actual->carnet <<'\t';
+            cout<< actual->nombre <<'\t';
+            cout<< actual->descripcion <<'\t';
+            cout<< actual->materia <<'\t';
+            cout<< actual->fecha <<'\t';
+            cout<< actual->hora <<'\t';
+            cout<< actual->estado <<endl;
+            actual = actual->der;
         }
     }
-
+    
     int sizeDeLaLista(){
         int contador = 1;
         if(inicio == NULL){
@@ -65,7 +65,8 @@ class ListaDoblementeEnlazada{
         }
         return contador;
     }
-    
+
+
     void borrarElemento(int valor){
         if(inicio == NULL){
             cout<<"La lista esta vacia"<<endl;
@@ -75,61 +76,127 @@ class ListaDoblementeEnlazada{
             inicio = inicio->der;
             inicio->izq == NULL;
             delete borrarInicio;
-        }
-        else if(valor == sizeDeLaLista()){
+
+        }else if(valor > sizeDeLaLista()){
+            cout<<"El numero ingresado es mayor a la cantidad de elementos en la lista"<<endl;
+
+        }else if(valor == sizeDeLaLista()){
             Nodo* borrarFin = fin;
             fin = fin->izq;
             fin->der = NULL;
             delete borrarFin;
 
         }else{
-            Nodo* reco = inicio;
+            Nodo* actual = inicio;
             Nodo* anterior;
             Nodo* siguiente;
             Nodo* borrar;
             int contador=1;
 
-            while(reco->der != NULL){
+            while(actual->der != NULL){
                 if(contador == valor){
-                    anterior=reco->izq;
-                    siguiente=reco->der;
-                    borrar = reco;
+                    anterior=actual->izq;
+                    siguiente=actual->der;
+                    borrar = actual;
                     anterior->der = siguiente;
                     siguiente->izq = anterior;
                     delete borrar;
                     break;
                 }
                 contador+=1;
-                reco=reco->der;
+                actual=actual->der;
             }
         }
     }
 
-    /*
-    void ModificarNodo(int valor){
-        int nuevoValor;
-        if(inicio == NULL){
-            cout<<"La lista esta vacia"<<endl;
-
-        }else if(fin->info == valor){
-            cout<<"Ingresar el nuevo valor: ";
-            cin>>nuevoValor;
-            fin->info = nuevoValor;
-            cout<<"valor modificado";
-
+    int valoresparaModificar(){
+        int opcion;
+        cout<<" ******* Opciones para modificar *******"<<endl;
+        cout<<"|              1- Carnet               |"<<endl;
+        cout<<"|              2- Nombre               |"<<endl;
+        cout<<"|              3- Descripcion          |"<<endl;
+        cout<<"|              4- Materia              |"<<endl;
+        cout<<"|              5- Fecha                |"<<endl;
+        cout<<"|              6- Hora                 |"<<endl;
+        cout<<"|              7- Estado               |"<<endl;
+        cout<<"Ingrese por teclado la opcion que desea modificar: ";
+        cin>>opcion;
+        if(opcion > 7){
+            cout<<"Opcion no valida"<<endl;
+            valoresparaModificar();
         }else{
-            Nodo* reco = inicio;
-            while(reco->der != NULL){
-                if(reco->info == valor){
-                    cout<<"Ingresar el nuevo valor: ";
-                    cin>>nuevoValor;
-                    reco->info = nuevoValor;
-                    cout<<"valor modificado";
-                    break;
+            return opcion;
+        }
+    }
+    
+
+    void ModificarNodo(int valor){
+        int opcion;
+        int carnetNuevo;
+        string nombreNuevo;
+        string descripcionNuevo;
+        string materiaNuevo;
+        string fechaNuevo;
+        string horaNuevo;
+        string estadoNuevo;
+
+        if(inicio == NULL){
+            cout<<"La lista se encuentra vacia";
+        }else{
+            Nodo* actual = inicio;
+            int contador = 1;
+            while(actual->der != NULL){
+                if(valor == contador){
+                    opcion = valoresparaModificar();
+                    switch (opcion){
+                    case 1:
+                        cout<<"Ingrese el nuevo valor del carnet: ";
+                        cin>>carnetNuevo;
+                        actual->carnet = carnetNuevo;
+                     break;
+
+                    case 2:
+                        cout<<"Ingrese el nuevo valor del nombre: ";
+                        cin>>nombreNuevo;
+                        actual->nombre = nombreNuevo;
+                     break;
+
+                    case 3:
+                        cout<<"Ingrese el nuevo valor del descripcion: ";
+                        cin>>descripcionNuevo;
+                        actual->descripcion = descripcionNuevo;
+                     break;
+
+                    case 4:
+                        cout<<"Ingrese el nuevo valor del materia: ";
+                        cin>>materiaNuevo;
+                        actual->materia = materiaNuevo;
+                     break;
+
+                    case 5:
+                        cout<<"Ingrese el nuevo valor del fecha: ";
+                        cin>>fechaNuevo;
+                        actual->fecha = fechaNuevo;
+                     break;
+
+                    case 6:
+                        cout<<"Ingrese el nuevo valor del hora: ";
+                        cin>>horaNuevo;
+                        actual->hora = horaNuevo;
+                     break;
+
+                    case 7:
+                        cout<<"Ingrese el nuevo valor del estado: ";
+                        cin>>estadoNuevo;
+                        actual->estado = estadoNuevo;
+                     break;
+
+                    }
                 }
-                reco=reco->der;
+                contador+=1;
+                actual=actual->der;
             }
         }
     }
-    */
+
 };
--- ListaDoble/principar.cpp
+++ ListaDoble/principar.cpp
@@ -10,15 +10,15 @@ int main(){
     ListaDoblementeEnlazada obj;
     
     obj.agregar(201801155,"cañas","chupar","inicio","hoy","ahorita","ansioso");
-    obj.agregar(201801155,"cañas","chupar","ninguna","hoy","ahorita","ansioso");
-    obj.agregar(201801155,"cañas","chupar","ninguna","hoy","ahorita","ansioso");
-    obj.agregar(201801155,"cañas","chupar","ninguna","hoy","ahorita","ansioso");
-    obj.agregar(201801155,"cañas","chupar","final","hoy","ahorita","ansioso");
+    obj.agregar(201801155,"cañas","chupar","segundo","hoy","ahorita","ansioso");
+    obj.agregar(201804420,"cañas","chupar","tercero","hoy","ahorita","ansioso");
+    obj.agregar(000000000,"cañas","chupar","cuarto","hoy","ahorita","ansioso");
+    obj.agregar(202068947,"cañas","chupar","final","hoy","ahorita","ansioso");
 
     obj.mostrar();
-
-    int ta = obj.sizeDeLaLista();
-    cout<<endl<<ta<<endl;
-
+    cout<<endl;
+    obj.ModificarNodo(4);
+    cout<<endl;
+    obj.mostrar();
     return 0;
 }
